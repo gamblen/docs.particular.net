4df7f5fe1d2f9d68fbac7abe4bfe11b49854fdfc
danielmarbach <daniel.marbach@openplace.net>
2020-03-12 16:45:53 +0100

    Some fiddling around

--- samples/msmq/simple/MsmqTransport_1/Sample/Program.cs
+++ samples/msmq/simple/MsmqTransport_1/Sample/Program.cs
@@ -10,18 +10,34 @@ class Program
         #region ConfigureMsmqEndpoint
 
         var endpointConfiguration = new EndpointConfiguration("Samples.Msmq.Simple");
-        var transport = endpointConfiguration.UseTransport<MsmqTransport>();
+        var transport = endpointConfiguration.UseTransport<AzureServiceBusTransport>();
+        transport.ConnectionString("");
+        transport.Transactions(TransportTransactionMode.ReceiveOnly);
+        transport.UseForwardingTopology();
+
+        endpointConfiguration.LimitMessageProcessingConcurrencyTo(100);
+
+        endpointConfiguration.UseSerialization<NewtonsoftSerializer>();
+
 
         #endregion
         endpointConfiguration.SendFailedMessagesTo("error");
+        endpointConfiguration.AuditProcessedMessagesTo("audit");
         endpointConfiguration.EnableInstallers();
         endpointConfiguration.UsePersistence<InMemoryPersistence>();
+        //var metrics = endpointConfiguration.EnableMetrics();
+        //metrics.SendMetricDataToServiceControl("Particular.Monitoring", TimeSpan.FromMilliseconds(100));
+
+        var recoverability = endpointConfiguration.Recoverability();
+        recoverability.AddUnrecoverableException<InvalidOperationException>();
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration)
             .ConfigureAwait(false);
-        var myMessage = new MyMessage();
-        await endpointInstance.SendLocal(myMessage)
-            .ConfigureAwait(false);
+        for (var i = 0; i < 10000; i++)
+        {
+            _ = endpointInstance.SendLocal(new MyMessage())
+                .ConfigureAwait(false);
+        }
         Console.WriteLine("Press any key to exit");
         Console.ReadKey();
         await endpointInstance.Stop()
